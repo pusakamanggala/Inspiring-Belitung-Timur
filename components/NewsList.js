2ebf12f4df7a549569f819340f095d2c1a8902c2
Pusaka Manggala <pusakamanggala@gmail.com>
2022-12-10 18:05:27 +0700

    feat : onPress

--- components/NewsList.js
+++ components/NewsList.js
@@ -3,18 +3,20 @@ import {View, Text, Image, TouchableOpacity} from 'react-native';
 
 const NewsList = props => {
   return (
-    <View style={{borderTopWidth: 1.5, borderTopColor: '#E0E0E0'}}>
-      <View style={{marginVertical: 20, flexDirection: 'row'}}>
-        <Image
-          style={{borderRadius: 10, overflow: 'hidden'}}
-          source={props.path}
-        />
-        <View style={{marginLeft: 15, width: '80%'}}>
-          <Text style={{fontSize: 18, color: '#1F2937'}}>{props.title}</Text>
-          <Text style={{color: '#9096A0'}}>{props.date}</Text>
+    <TouchableOpacity onPress={props.onPress}>
+      <View style={{borderTopWidth: 1.5, borderTopColor: '#E0E0E0'}}>
+        <View style={{marginVertical: 20, flexDirection: 'row'}}>
+          <Image
+            style={{borderRadius: 10, overflow: 'hidden'}}
+            source={props.path}
+          />
+          <View style={{marginLeft: 15, width: '80%'}}>
+            <Text style={{fontSize: 18, color: '#1F2937'}}>{props.title}</Text>
+            <Text style={{color: '#9096A0'}}>{props.date}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
